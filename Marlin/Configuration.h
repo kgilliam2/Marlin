6f4c788250ffa68e1c4b0d827b3a0575e1703dfb
Kevin Gilliam <gilliam2@gmail.com>
2020-11-28 21:42:09 -0700

    added alloy910 preheat

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2062,6 +2062,10 @@
 #define PREHEAT_2_TEMP_CHAMBER 35
 #define PREHEAT_2_FAN_SPEED     0 // Value from 0 to 255
 
+#define PREHEAT_3_LABEL       "ALLOY910"
+#define PREHEAT_3_TEMP_HOTEND 245
+#define PREHEAT_3_TEMP_BED    45
+#define PREHEAT_3_FAN_SPEED     0 // Value from 0 to 255
 /**
  * Nozzle Park
  *
